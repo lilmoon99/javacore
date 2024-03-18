3b82a4745f4cf45a7ca5f7aaea1d986fea03eaa1
lilmoon99 <oxygen4ik@bk.ru>
2024-03-18 19:37:07 +0400

    added employee class

---
+++ java_core/lesson3_hw/src/main/java/ru/lilmoon/Employee.java
@@ -0,0 +1,84 @@
+package ru.lilmoon;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class Employee {
+    private String surname;
+    private String name;
+    private String middleName;
+    private String position;
+    private String phoneNumber;
+    private int salary;
+    private LocalDate birthDate;
+
+    public Employee(String surname, String name, String middleName, String position, String phoneNumber, int salary, LocalDate birthDate) {
+        this.surname = surname;
+        this.name = name;
+        this.middleName = middleName;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.setBirthDate(birthDate);
+    }
+
+    public int getAge() {
+        return this.birthDate == null ? -1 : (int) this.birthDate.until(LocalDate.now(), ChronoUnit.YEARS);
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        if (birthDate == null || birthDate.isAfter(LocalDate.now().minusYears(14))) {
+            System.out.println("Дата рождения работника задана некорректно!");
+        } else {
+            this.birthDate = birthDate;
+        }
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
